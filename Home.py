a0e8d2e49a74f3cfc41c677fe7a4401cfa6e4c8f
ahmadkayyali-snowflake <ahmad.al-kayyali@snowfkake.com>
2025-02-14 17:08:24 +1100

    page names

--- Home.py
+++ Home.py
@@ -74,7 +74,7 @@ st.markdown(
         text-align: center;
         box-shadow: 2px 2px 10px rgba(255, 255, 255, 0.1);
     ">
-        <h1 style=" font-size: 42px; font-weight: 700; margin-bottom: 10px;">
+        <h1 style="color:white; font-size: 42px; font-weight: 700; margin-bottom: 10px;">
         Snowflake Performance Analytics
         </h1>
         <p style="color: white; font-size: 18px; font-weight: 400; max-width: 800px; margin: auto;">
@@ -96,8 +96,8 @@ with col1:
         """
         <div class="card">
             <div class="card-icon">💰</div>
-            <h3>Credit Usage</h3>
-            <p style="color:white;">Monitor credit consumption trends, usage over time, and remaining balances.</p>
+            <h3 style="color: white;">Credit Usage</h3>
+            <p style="color:whrite;">Monitor credit consumption trends, usage over time, and remaining balances.</p>
         </div>
         """,
         unsafe_allow_html=True,
@@ -110,7 +110,7 @@ with col2:
         """
         <div class="card">
             <div class="card-icon">📈</div>
-            <h3>Query Monitoring</h3>
+            <h3 style="color: white;">Query Monitoring</h3>
             <p style="color:white;">Analyze query performance by status, execution time, and failed queries.</p>
         </div>
         """,
@@ -124,14 +124,14 @@ with col3:
         """
         <div class="card">
             <div class="card-icon">🛠️</div>
-            <h3>Tasks & Tables</h3>
+            <h3 style="color: white;">Tasks & Tables</h3>
             <p style="color:white;">Track task refreshes, lag differences, and identify failed tasks & tables.</p>
         </div>
         """,
         unsafe_allow_html=True,
     )
-    if st.button("Go to Tasks & Tables", key="tasks", use_container_width=True):
-        st.switch_page("pages/Tasks.py")
+    if st.button("Go to Tasks & Dynamic Tables", key="tasks", use_container_width=True):
+        st.switch_page("pages/Tasks_&_Dynamic_Tables.py")
 
 st.markdown("---")
 
@@ -142,7 +142,7 @@ with col4:
         """
         <div class="card">
             <div class="card-icon">📡</div>
-            <h3>Data Feeds</h3>
+            <h3 style="color: white;">Data Feeds</h3>
             <p style="color:white;">Monitor query volume, credit consumption, and unique user trends.</p>
         </div>
         """,
@@ -156,7 +156,7 @@ with col5:
         """
         <div class="card">
             <div class="card-icon">🚀</div>
-            <h3>SiS Performance</h3>
+            <h3 style="color: white;">SiS Performance</h3>
             <p style="color:white;">Analyze SiS queries, success rates, and overall system efficiency.</p>
         </div>
         """,
